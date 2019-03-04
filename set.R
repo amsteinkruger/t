99919d21a5c6b8f5b60c41306af6b977c953eaab
Andrew Steinkruger <asteinkruger@bren.ucsb.edu>
2019-03-04 13:22:08 -0800

    Four steps away.

---
+++ set.R
@@ -0,0 +1,33 @@
+# ---- set ----
+
+# Market
+#  Estimate inverse demand.
+nlm = nls(p ~ q * a + (g ^ b) + c, data = ma_dat, start = c(a = -5.00, b = 2.00, c = 20))
+#  Clean results.
+nlm_tidy = tidy(nlm)
+
+# Aquaculture
+#  Estimate incremental mortalities.
+#  Regression:
+am_reg = nls(m_months ~ b1 * exp(b2 * a_months), aq_mort_dat, start = list(b1 = 8.00, b2 = - 1.00))
+#  Clean results.
+am_reg_tidy = tidy(am_reg)
+
+
+# Pull initial and intermediate parameters together.
+pars_full = pars %>%
+  # Add market outputs to parameter table.
+  add_row(name_long = "Quantity Elasticity", name_short = "a_ma", "function" = "Demand", default = nlm_tidy$estimate[1], pessimistic = nlm_tidy$estimate[1] - nlm_tidy$std.error[1], optimistic = nlm_tidy$estimate[1] + nlm_tidy$std.error[1], units = "-", module = "Fishery", source_default = "Intermediate", source_pess = NA, sources_opt = NA) %>% 
+  add_row(name_long = "Size Premium", name_short = "b_ma", "function" = "Demand", default = nlm_tidy$estimate[2], pessimistic = nlm_tidy$estimate[2] - nlm_tidy$std.error[2], optimistic = nlm_tidy$estimate[2] + nlm_tidy$std.error[2], units = "-", module = "Fishery", source_default = "Intermediate", source_pess = NA, sources_opt = NA) %>%
+  add_row(name_long = "Choke Price", name_short = "c_ma", "function" = "Demand", default = nlm_tidy$estimate[3], pessimistic = nlm_tidy$estimate[3] - nlm_tidy$std.error[3], optimistic = nlm_tidy$estimate[3] + nlm_tidy$std.error[3], units = "-", module = "Fishery", source_default = "Intermediate", source_pess = NA, sources_opt = NA) %>%
+  # Add aquaculture outputs to parameter table.
+  add_row(name_long = "Aq. Mortality Coefficient", name_short = "b1_mort_aq", "function" = "Aquaculture Mortality", default = am_reg_tidy$estimate[1], pessimistic = am_reg_tidy$estimate[1] + am_reg_tidy$std.error[1], optimistic = am_reg_tidy$estimate[1] - am_reg_tidy$std.error[1], units = "-", module = "Aquaculture", source_default = "Intermediate", source_pess = NA, sources_opt = NA) %>%
+  add_row(name_long = "Aq. Mortality Coefficient", name_short = "b2_mort_aq", "function" = "Aquaculture Mortality", default = am_reg_tidy$estimate[2], pessimistic = am_reg_tidy$estimate[2] + am_reg_tidy$std.error[2], optimistic = am_reg_tidy$estimate[2] - am_reg_tidy$std.error[2], units = "-", module = "Aquaculture", source_default = "Intermediate", source_pess = NA, sources_opt = NA)
+
+    
+# Turn parameters into a matrix for multiple model runs.
+pars = pars_full %>% 
+  select(2, 4:6) %>%
+  column_to_rownames(var = "name_short")
+
+# Build out a matrix of parameters for sensitivity analysis.
