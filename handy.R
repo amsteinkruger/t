d9b4ce6e6600f8933e9375dd015af5d7d1820e06
Miguel Castellanos <mcastellanos@ESM.UCSB.EDU>
2019-03-18 16:53:56 -0700

    MW Separated output and proft graphs, need to stack buche and meat output

--- handy.R
+++ handy.R
@@ -5,16 +5,36 @@
 #write.csv(results_handy, "results_handy.csv")
 
 # without fixing prices + miscellaneous other tweaks:
-results_handy_dat = read_csv("results_handy.csv")
 
-plot_handy = 
-  ggplot(results_handy_dat) +
-  geom_col(aes(Year, Result, fill = Variable)) +
+#library(pubr)
+#library(gridExtra)
+
+results_output <- read_csv("~/GitHub/t/results_output.csv")
+results_profit <- read_csv("~/GitHub/t/results_profit.csv")
+
+
+plot_output = 
+  ggplot(results_output) +
+  geom_col(aes(Year, Result), fill = "red") +  #fill = Variable
   scale_fill_brewer(palette = "Set1") +
-  facet_wrap(~Variable) +
+  #facet_wrap(~Variable) +
   scale_y_continuous(labels = scales::comma) +
   scale_x_continuous(expand = c(0, 0)) +
-  labs(y = "") +
+  labs(y = "Kilograms") +
   theme_classic()
 
 #print(plot_handy)
+
+plot_output
+
+plot_profit = 
+  ggplot(results_profit) +
+  geom_col(aes(Year, Result), fill = "blue") + #fill = Variable
+  scale_fill_brewer(palette = "Set1") +
+  #facet_wrap(~Variable) +
+  scale_y_continuous(labels = scales::comma) +
+  scale_x_continuous(expand = c(0, 0)) +
+  labs(y = "USD 2018") +
+  theme_classic()
+
+plot_profit
