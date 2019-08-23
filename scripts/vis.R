f79547260a0e76d45e6ac6bd8448fd8e5ceea744
amsteinkruger <asteinkruger@bren.ucsb.edu>
2019-08-23 18:57:36 -0400

    unsure what's going on

--- scripts/vis.R
+++ scripts/vis.R
@@ -16,12 +16,17 @@
 #font_add("avenir", "avenir.otf")
 #showtext_auto()
 
-# Stock and catch of reproductive biomass in numbers and mass.
+# Summarize biomass.
 results_sum =  results %>% 
   #na.omit() %>% # This is a Band-Aid to filter out any surprise NAs. Swap out for a better solution in set.R.
   #filter(Age < 3) %>% # This is a quick trick to filter out juveniles and subadults and keep reproductive biomass.
   filter(Variable == "Numbers") %>% 
-  mutate(Biomass = fun_l_w(pars$default[4], fun_a_l(Age - 0.5, pars$default[1], pars$default[2], pars$default[3]),  pars$default[5]) / 1000 * Result) %>% 
+  mutate(Biomass = fun_l_w(pars_base$default[4], 
+                           fun_a_l(Age - 0.5, 
+                                   pars_base$default[1], 
+                                   pars_base$default[2], 
+                                   pars_base$default[3]),  
+                           pars_base$default[5]) / 1000 * Result) %>% 
   group_by(Year, 
            Variable, 
            Run, 
@@ -32,12 +37,16 @@ results_sum =  results %>%
   ungroup() %>% 
   mutate(Run = as.factor(Run))
 
-# Ditto, but by means of runs.
+# Summarize biomass by cages and quantiles.
 results_summer = results_sum %>% 
   mutate(Cages = ifelse(Cages > 0, 
                         ifelse(Cages > 25, 
-                                      "25 - 50", 
-                                      "1 - 25"),
+                               ifelse(Cages > 50, 
+                                      ifelse(Cages > 75, 
+                                             "75 - 100", 
+                                             "50 - 75"), 
+                                      "25 - 50"), 
+                               "1 - 25"),
                         "0")) %>% 
   group_by(Year, 
            Variable, 
@@ -75,18 +84,35 @@ plot_n_lin =
                   ymax = SevBio),
               alpha = 0.50) +
   geom_ribbon(data = filter(results_summer, 
-                            Variable == "Numbers"),
+                            Variable == "Numbers",
+                            Scenario == "Counterfactual"),
               aes(x = Year + 2016,
                   ymin = ForBio, 
                   ymax = SixBio,
+                  color = Cages,
                   group = Cages,
                   fill = Cages),
+              alpha = 0.50) +
+  geom_ribbon(data = filter(results_summer, 
+                            Variable == "Numbers",
+                            Scenario == "Status Quo"),
+              aes(x = Year + 2016,
+                  ymin = ForBio, 
+                  ymax = SixBio),
               alpha = 0.75) +
-  #scale_fill_viridis_c(option = "A") +
+  scale_color_manual(values = c("steelblue1", 
+                                "steelblue2", 
+                                "steelblue3", 
+                                "steelblue4")) +
+  scale_fill_manual(values = c("steelblue1", 
+                               "steelblue2", 
+                               "steelblue3", 
+                               "steelblue4")) +
   scale_y_continuous(expand = c(0, 0),
                      limits = c(0, 15000),
                      labels = scales::comma) + 
-  scale_x_continuous(expand = c(0, 0.25)) +  
+  scale_x_continuous(breaks = c(2017, 2022, 2027),
+                     expand = c(0, 0.75)) +  
   labs(x = "Year", y = "Biomass (Tonnes)") + 
   theme_classic() + #base_family = "avenir"
   theme(panel.grid.major = element_blank(), 
@@ -146,7 +172,65 @@ plot_pi_neg =
 # Put in a tricky bit of grid work here for overlapping plots. Or not.
 
 # Data wrangling for differences.
+results_sum_pi_dif = 
+  results %>% 
+  select(-Age, -Group) %>% 
+  filter(Variable == "Aquaculture Profit" |
+         Variable == "Poaching Profit") %>% 
+  mutate(Variable = str_remove(string = Variable, 
+                               pattern = " Profit")) %>%  
+  mutate(Result = Result * 0.000001) %>%
+  mutate(Cages = ifelse(Cages > 0, 
+                        ifelse(Cages > 25, 
+                               ifelse(Cages > 50, 
+                                      ifelse(Cages > 75, 
+                                             "75 - 100", 
+                                             "50 - 75"), 
+                                      "25 - 50"), 
+                               "1 - 25"),
+                        "0")) %>% 
+  na.omit() %>% # Track down origin of NAs.
+  group_by(Year, 
+           Run,
+           Variable, 
+           Scenario,
+           Cages) %>% 
+  mutate(Result = ifelse(Scenario == "Counterfactual", 
+                         Result, 
+                         -Result)) %>% 
+  ungroup() %>% 
+  group_by(Year,
+           Run,
+           Variable,
+           Cages) %>% 
+  summarize(Difference = sum(Result)) %>% 
+  ungroup() %>% 
+  group_by(Year, 
+           Variable,
+           Cages) %>% 
+  summarize(Med = median(Difference),
+            Mea = mean(Difference))
 
+
+# Plot differences.
+plot_pi_dif = 
+  ggplot(data = results_sum_pi_dif) + 
+  geom_col(aes(x = Year + 2016,
+               y = Mea,
+               fill = Cages),
+           position = "dodge") +
+  geom_hline(aes(yintercept = 0),
+             color = "firebrick",
+             linetype = "dashed") +
+  scale_fill_manual(values = c("steelblue1", 
+                               "steelblue2", 
+                               "steelblue3", 
+                               "steelblue4")) +
+  labs(x = "", y = "\u0394 \u03C0 (US$M 2018)") +
+  scale_x_continuous(breaks = c(2017, 2022, 2027),
+                     expand = c(0, 0.75)) + 
+  theme_classic() +
+  facet_wrap(~Variable)
 
 # Data wrangling for production.
 results_sum_pr = results %>% 
@@ -172,7 +256,7 @@ plot_pr =
 results_sum_p = results %>% 
   filter(Variable == "Price")
 
-# Production for both scenarios.
+# Prices for both scenarios.
 plot_p = 
   ggplot(results_sum_p, 
          aes(x = factor(Year + 2016), 
@@ -193,12 +277,6 @@ plot_p =
   facet_wrap(~Scenario)
 
 # Principal Component Analysis of parameters on biomass in final year.
-#  Watch out for plyr-dplyr conflict in loading these libraries - fix wrt key.Rmd (1).
-#library(devtools)
-#install_github("vqv/ggbiplot")
-library(ggbiplot)
-library(factoextra)
-
 # Wrangle parameter sets for association with corresponding runs.
 pars_pca = pars %>% 
   select(7:length(pars)) %>% 
@@ -277,7 +355,6 @@ ggsave("plot_p.png",
 #       height = 6.5)
 
 # Save tables, too.
-#library(kableExtra)
 # Importances of PCs.
 #kable(pca_imp, "html") %>%
 #  cat(., file = "pca_imp.html")
@@ -286,7 +363,4 @@ ggsave("plot_p.png",
 #  cat(., file = "pca_rot.html")
 
 print(plot_n_lin)
-print(plot_pi)
-print(plot_pr)
-print(plot_p)
 #print(plot_pc)
