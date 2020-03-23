74730dbf60e88d48d76347e82399e40c4f5a2e9d
amsteinkruger <asteinkruger@bren.ucsb.edu>
2020-03-22 22:44:39 -0400

    Figure summarizing all outcomes in panels.

--- scripts/bio_mat.R
+++ scripts/bio_mat.R
@@ -2,7 +2,8 @@
 
 # Summarize results for matrix panels.
 #  Find median biomass for each cohort and year.
-results_mat = results %>% 
+results_mat = 
+  results %>% 
   filter(Variable == "Numbers") %>% 
   mutate(Biomass = fun_l_w(pars_base["a_lw", 1], 
                            fun_a_l(Age - 0.5, 
@@ -18,58 +19,61 @@ results_mat = results %>%
   ungroup()
 
 #  Get median biomasses of first year by age for proportional numbers.
-results_bas = results_mat %>% 
-  filter(Year == 1) %>% 
-  select(Age, 
+results_bas = 
+  results_mat %>% 
+  filter(Year == 1 & Scenario == "Status Quo") %>% 
+  select(Age,
          BasNum = MedNum, 
          BasBio = MedBio)
 
-#  Calculate proportions.
-results_mat = results_mat %>% 
+#  Calculate proportions and difference of proportions.
+results_mat = 
+  results_mat %>% 
   left_join(results_bas, by = "Age") %>% 
   mutate(ProNum = MedNum / BasNum,
-         ProBio = MedBio / BasBio)
+         ProBio = MedBio / BasBio) %>% 
+  filter(Scenario == "Status Quo" | Scenario == "Aquaculture Intervention") %>% 
+  select(Year, Age, Scenario, ProBio) %>% 
+  pivot_wider(names_from = Scenario,
+              values_from = ProBio) %>% 
+  mutate(Difference = `Aquaculture Intervention` - `Status Quo`) %>% 
+  pivot_longer(cols = c("Aquaculture Intervention", "Status Quo", "Difference"),
+               names_to = "Scenario",
+               values_to = "Values") %>% 
+  mutate(Scenario_f = factor(Scenario,
+                             levels = c("Status Quo", 
+                                        "Aquaculture Intervention", 
+                                        "Difference"), 
+                             labels = c("Status Quo", 
+                                        "Aquaculture Intervention", 
+                                        "Difference")))
 
 # Plot matrices.
-plot_bio_mat_medbio = 
-  ggplot() +
-  geom_tile(data = results_mat,
-            aes(x = Year,
-                y = Age, 
-                fill = MedBio)) +
-  scale_fill_viridis_c(option = "D") +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  theme_classic() +
-  facet_wrap(~Scenario)
-
-plot_bio_mat_pronum = 
+plot_bio_mat = 
   ggplot() +
   geom_tile(data = results_mat,
             aes(x = Year + 2016,
                 y = Age, 
-                fill = ProNum)) +
+                fill = Values)) +
   scale_fill_viridis_c(option = "D") +
   scale_x_continuous(expand = c(0, 0),
-                     breaks = c(2017, 2036)) +
+                     breaks = c(2019, 2029)) +
   scale_y_continuous(expand = c(0, 0)) +
-  labs(x = "", y = "Age") +
-  theme_classic() +
-  theme(legend.background = element_rect(fill = "transparent"),
-        legend.title = element_blank(),
-        strip.background = element_blank(),
-        #strip.text = element_blank(),
-        panel.background = element_rect(fill = "transparent", color = NA),
-        panel.spacing.x = unit(5, "lines"),
-        plot.background = element_rect(fill = "transparent", color = NA)) + 
-  facet_wrap(~Scenario)
+  labs(x = "", 
+       y = "Age",
+       fill = "% of 2017 Biomass") +
+  theme_pubr() +
+  guides(fill = guide_colorbar(barwidth = 15,
+                               barheight = 0.5,
+                               ticks = FALSE)) +
+  facet_wrap(~Scenario_f)
 
-print(plot_bio_mat_pronum)
+print(plot_bio_mat)
 
 # Save.
-ggsave("./out/plot_mat.png", 
-       plot_bio_mat_pronum,
+ggsave("plot_mat.png", 
+       plot_bio_mat,
        dpi = 300,
        bg = "transparent",
-       width = 9.00, 
-       height = 4.13)
+       width = 11, 
+       height = 7)
